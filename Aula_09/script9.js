486ed4c7e4dfd30a9d28471f2122ff2b89ea4244
alexmeirelles <alemeirelles@msn.com>
2022-01-28 23:02:42 -0300

    atu

---
+++ Aula_09/script9.js
@@ -0,0 +1,53 @@
+var prompt = require('prompt-sync')();
+
+
+/*1.	Imprimir números de 1 a 10.
+for (i = 0; i < 11; i++)
+console.log(i)
+
+2.	Imprimir a tabuada do número 8.
+for (i = 0; i < 11; i++)
+console.log(8*i)
+
+3.	Imprimir a soma dos números de 1 a 10.
+
+for(let i = 1; i < 11; i++){
+    soma += i
+}
+console.log(soma)
+
+4.	Retornar a maior string de um array.
+
+const prompt = require('prompt-sync')();
+const filmes = ['Batman', 'Deadpool', 'Shadow of Colossus'];
+let maior='';
+for (let i = 0; i < filmes.length; i++) {
+   if ( filmes[i] > maior ) {
+      maior = filmes[i];
+   }
+}
+console.log(maior)
+
+
+5.	Localizar o maior valor dentro de um array de números.
+
+const prompt = require('prompt-sync')();
+const filmes = [1, 2, 3, 4, 5, 6, 7];
+let maior='';
+for (let i = 0; i < filmes.length; i++) {
+   if ( filmes[i] > maior ) {
+      maior = filmes[i];
+   }
+}
+console.log(maior)
+
+6.	Calcular a soma dos números de um array.
+
+
+
+7.	Calcular a média de todos os números de um array.
+
+
+*/
+
+
